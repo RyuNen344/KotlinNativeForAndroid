aff13edade6b27fbd6ad7fd8c386412bab996e64
RyuNen344 <s1100633@outlook.com>
2022-07-19 01:51:44 +0900

    optimize ndk

--- androidApp/src/main/cpp/ndk-wrapper.cpp
+++ androidApp/src/main/cpp/ndk-wrapper.cpp
@@ -2,7 +2,11 @@
 #include <string>
 #include "libshared_api.h"
 
-long long fibonacciNative(long long x);
+long fibonacciNative(long x) {
+    if (x <= 0) return 0;
+    if (x == 1) return 1;
+    return fibonacciNative(x - 2) + fibonacciNative(x - 1);
+}
 
 extern "C" JNIEXPORT jlong JNICALL
 Java_com_ryunen344_kotlin_android_android_NdkWrapper_fibonacciNative(
@@ -19,10 +23,4 @@ Java_com_ryunen344_kotlin_android_android_NdkWrapper_fibonacciKNative(
         jlong count) {
     libshared_ExportedSymbols *symbols = libshared_symbols();
     return symbols->kotlin.root.com.ryunen344.kotlin.android.fibonacci(count);
-}
-
-long long fibonacciNative(long long x) {
-    if (x <= 0) return 0;
-    if (x == 1) return 1;
-    return fibonacciNative(x - 1) + fibonacciNative(x - 2);
 }
